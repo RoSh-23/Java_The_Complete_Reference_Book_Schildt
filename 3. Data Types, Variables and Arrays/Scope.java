257f4b8aec57d9b84cb1dc24aa86f7a22ce7ff80
Rohan Shah <shah_rohan2001@yahoo.com>
2022-09-05 18:37:15 +0530

    add chapter 3

---
+++ 3. Data Types, Variables and Arrays/Scope.java
@@ -0,0 +1,19 @@
+//	demonstrate block scope 
+class Scope
+{
+	public static void main(String[] args)
+	{	
+		int x;
+		x = 10;
+		if(x == 10)
+		{
+			int y = 20;
+			System.out.println("x and y: " + x + " " + y);
+			x = y * 2;
+		}
+		// y = 100; // y not known here
+
+		// x still known here 
+		System.out.println("x is " + x);
+	}
+}
---
+++ 3. Data Types, Variables and Arrays/ScopeErr.java
@@ -0,0 +1,11 @@
+// not complie 
+class ScopeErr 
+{
+	public static void main(String[] args)
+	{
+		int bar = 1;
+		{
+			int bar = 2; // new scope 
+		}
+	}
+}
