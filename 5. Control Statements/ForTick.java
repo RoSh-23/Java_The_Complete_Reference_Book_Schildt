ef8013cb1dae72106f9936e4586f02de39caa97b
Rohan Shah <shah_rohan2001@yahoo.com>
2022-09-09 18:07:37 +0530

    add chapter 5

---
+++ 5. Control Statements/ForTick.java
@@ -0,0 +1,20 @@
+// demonstrate the for loop.
+class ForTick
+{
+	public static void main(String[] args)
+	{
+		/*
+		int n;
+		for(n = 10; n > 0; n--)
+		{
+			System.out.println("tick " + n);
+		}
+		*/
+		/* OR */ 
+		// here n is declared inside of the for loop
+		for(int n = 10; n > 0; n--)
+		{
+			System.out.println("tick " + n);
+		}
+	}
+}
