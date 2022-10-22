afb24fa6c205623d9bef870dae2ffc3cb8e395c3
Rohan Shah <shah_rohan2001@yahoo.com>
2022-10-22 12:05:14 +0530

    Add Chapter 11

---
+++ 11. Multithreaded Programming/ThreadDemo.java
@@ -0,0 +1,53 @@
+// create a second thread.
+class NewThread implements Runnable 
+{
+	Thread t;
+
+	NewThread()
+	{
+		// Create a new, second thread
+		t = new Thread(this, "Demo Thread");
+		System.out.println("Child thread: " + t);
+	}
+
+	// this is the entry point for the second thread.
+	public void run()
+	{
+		try
+		{
+			for(int i = 5; i > 0; i--)
+			{
+				System.out.println("Child Thread: " + i);
+				Thread.sleep(500);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Child interrupted.");
+		}https://en.wikipedia.org/wiki/Advanced_Message_Queuing_Protocol
+		System.out.println("Exiting child thread.");
+	}
+}
+
+class ThreadDemo
+{
+	public static void main(String args[])
+	{
+		NewThread nt = new NewThread();	// create a new thread 
+		nt.t.start();	// Start the thread 
+
+		try
+		{
+			for(int i = 5; i > 0; i--)
+			{
+				System.out.println("Main Thread: " + i);
+				Thread.sleep(1000);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Main thread interrupted.");
+		}
+		System.out.println("Main thread exiting.");
+	}
+}
